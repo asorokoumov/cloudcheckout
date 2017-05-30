e99d31e34d253adbafe458218998c23d846688c6
Sorokoumov Anton <sorokoumov.anton@gmail.com>
2017-05-30 19:55:54 +0300

    Add Seller, Product, Delivery models

--- checkout/models.py
+++ checkout/models.py
@@ -5,10 +5,35 @@ from django.utils import timezone
 
 
 class Seller (models.Model):
-    name = models.CharField(max_length=200)
-    email = models.CharField(max_length=200)
+    login = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
     instagram = models.CharField(max_length=200)
-    created_date = models.DateTimeField(default=timezone.now)
+    email = models.CharField(max_length=200)
+    phone = models.CharField(max_length=200)
 
     def __str__(self):
-        return self.name
+        return self.login
+
+
+class Product (models.Model):
+    seller = models.ForeignKey(Seller)
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    price = models.FloatField()
+    image = models.CharField(max_length=200)
+    link = models.CharField(max_length=200)
+    development = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
+
+
+class Delivery(models.Model):
+    product = models.ForeignKey(Product)
+    delivery_service = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    price = models.FloatField()
+
+    def __str__(self):
+        return self.delivery_service
+
