e5cedad7cab8467a227f232c9c61d42fc004b993
Sorokoumov Anton <sorokoumov.anton@gmail.com>
2017-05-30 20:06:54 +0300

    Register Product and Delivery models

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Seller
+from .models import Seller, Product, Delivery
 
 # Register your models here.
 
 admin.site.register(Seller)
+admin.site.register(Product)
+admin.site.register(Delivery)
