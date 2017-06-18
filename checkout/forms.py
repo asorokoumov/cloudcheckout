2e32cfbe6ba6af5f40f335cf97892858d54ba719
Sorokoumov Anton <sorokoumov.anton@gmail.com>
2017-06-18 21:08:53 +0300

    Add new design for Contacts page

--- checkout/forms.py
+++ checkout/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.forms import Textarea
+from django.forms.widgets import Input
 
 from .models import Customer, Order
 
@@ -8,6 +9,12 @@ class ContactsForm(forms.ModelForm):
     class Meta:
         model = Customer
         fields = ('name', 'phone', 'email', 'comments')
+        widgets = {
+            'comments': Textarea(attrs={'class': 'textarea', 'placeholder': 'Комментарий'}),
+            'phone': Input(attrs={'class': 'input','placeholder': 'Телефон', 'type': 'tel'}),
+            'email': Input(attrs={'class': 'input', 'placeholder': 'Email', 'type': 'email'}),
+            'name': Input(attrs={'class': 'input', 'placeholder': 'Имя', 'type': 'text'}),
+        }
 
 
 class ProductForm(forms.ModelForm):
