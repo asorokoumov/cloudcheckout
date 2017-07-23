dff76003b5720994a47dac1397f73640d2703194
Sorokoumov Anton <sorokoumov.anton@gmail.com>
2017-07-23 10:30:52 +0300

    Make comments field optional

--- checkout/models.py
+++ checkout/models.py
@@ -30,7 +30,7 @@ class Customer(models.Model):
     name = models.CharField(max_length=200)
     phone = models.CharField(max_length=200)
     email = models.CharField(max_length=200)
-    comments = models.TextField()
+    comments = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.name
