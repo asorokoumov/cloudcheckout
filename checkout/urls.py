c6042d932d013cf0e50bce0342e96f44d5cf75c0
Sorokoumov Anton <sorokoumov.anton@gmail.com>
2018-04-04 18:51:17 +0300

    Postpone sellerpage development

--- checkout/urls.py
+++ checkout/urls.py
@@ -9,14 +9,14 @@ urlpatterns = [
     url(r'^(?P<order_nr>[-\w]+)/production/$', views.checkout_production, name='checkout_production'),
     url(r'^(?P<order_nr>[-\w]+)/delivery/$', views.checkout_delivery, name='checkout_delivery'),
     url(r'^(?P<order_nr>[-\w]+)/success/$', views.checkout_success, name='checkout_success'),
-    url(r'^seller/login/$', views.seller_login, name='seller_login'),
-    url(r'^seller/logout/$', views.seller_logout, name='seller_logout'),
-    url(r'^seller/register/$', views.seller_register, name='seller_register'),
-    url(r'^seller/products/add/$', views.seller_products_add, name='seller_products_add'),
-    url(r'^seller/products/$', views.seller_products, name='seller_products'),
-    url(r'^seller/products/edit/(?P<product_link>[-@./#&+\w\s]+)/$', views.seller_products_edit, name='seller_products_edit'),
-    url(r'^seller/products/delete/(?P<product_link>[-@./#&+\w\s]+)/$', views.seller_products_delete, name='seller_products_delete'),
-    url(r'^seller/$', views.seller_space, name='seller_space'),
+#    url(r'^seller/login/$', views.seller_login, name='seller_login'),
+#    url(r'^seller/logout/$', views.seller_logout, name='seller_logout'),
+#    url(r'^seller/register/$', views.seller_register, name='seller_register'),
+#    url(r'^seller/products/add/$', views.seller_products_add, name='seller_products_add'),
+#    url(r'^seller/products/$', views.seller_products, name='seller_products'),
+#    url(r'^seller/products/edit/(?P<product_link>[-@./#&+\w\s]+)/$', views.seller_products_edit, name='seller_products_edit'),
+#    url(r'^seller/products/delete/(?P<product_link>[-@./#&+\w\s]+)/$', views.seller_products_delete, name='seller_products_delete'),
+#    url(r'^seller/$', views.seller_space, name='seller_space'),
 
     #TODO add default path
 ]
